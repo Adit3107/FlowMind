40a94a94a6e42b10b13e85189170a954526d020f
Aditya Kalmegh <adityakalmegh01@gmail.com>
2026-06-25 02:57:12 +0530

    calendar added successfully

--- db/schema.ts
+++ db/schema.ts
@@ -10,5 +10,22 @@ export const users = pgTable("users", {
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 });
 
+export const tasks = pgTable("tasks", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull(),
+  title: text("title").notNull(),
+  description: text("description"),
+  date: timestamp("date"), // Null for drafts
+  category: text("category").default("Work").notNull(),
+  color: text("color").default("#f15f49").notNull(),
+  type: text("type").default("task").notNull(),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
+
+export type Task = typeof tasks.$inferSelect;
+export type NewTask = typeof tasks.$inferInsert;
+
